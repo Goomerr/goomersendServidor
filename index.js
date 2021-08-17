827741ba5f70fc814b56feaeb14d94fa80563c9d
Goomerr <63950956+Goomerr@users.noreply.github.com>
2021-08-17 15:41:10 +0200

    Cambio index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ conectarDB();
 console.log('Comenzando con GoomerSend');
 
 //Habilitar cors
-const opcionesCors = {
-    origin: process.env.FRONTEND_URL
-}
-app.use(cors(opcionesCors));
+// const opcionesCors = {
+//     origin: process.env.FRONTEND_URL
+// }
+app.use(cors());
 
 //Puerto de la app
 const port = process.env.PORT || 4000;
